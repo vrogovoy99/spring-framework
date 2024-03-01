0b11e6c07391a603660db8730c3e4edfd323f22c
Human <vladislav.rogovoy@gmail.com>
2024-03-01 17:24:36 -0500

    multiple beans using the same class

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -1,6 +1,6 @@
 package com.cydeo.bean_annotation;
 
-import lombok.Builder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
