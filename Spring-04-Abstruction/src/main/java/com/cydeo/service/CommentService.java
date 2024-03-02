fba3cffc9cfa4c90db23c785165f77303d2401c0
Human <vladislav.rogovoy@gmail.com>
2024-03-02 10:27:27 -0500

    04-Abstraction modules are created

---
+++ Spring-04-Abstruction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,21 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+
+
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
