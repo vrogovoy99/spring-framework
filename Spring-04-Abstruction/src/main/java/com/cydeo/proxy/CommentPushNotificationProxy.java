2ae0fe71f3dfb199396353262cf7a2cf38dc326b
Human <vladislav.rogovoy@gmail.com>
2024-03-02 13:05:38 -0500

    04-Abstraction modules completed, @qualifier tests added

--- Spring-04-Abstruction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
+++ Spring-04-Abstruction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
@@ -2,13 +2,15 @@ package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("EMAIL")
-public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
-        System.out.println(("Email notification for comment : " + comment.getText()));
+        System.out.println("Sending push notification for comment : " +comment.getText());
+
     }
 }
