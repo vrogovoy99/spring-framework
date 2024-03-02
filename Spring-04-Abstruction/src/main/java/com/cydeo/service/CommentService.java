c0a0121d3b6511e1f6c5919aa54160a25bad3724
Human <vladislav.rogovoy@gmail.com>
2024-03-02 10:55:33 -0500

    04-Abstraction modules completed

--- Spring-04-Abstruction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-04-Abstruction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,8 +3,9 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
