d056b85449c3a4bc80fd6b2d832be688eec47067
Human <vladislav.rogovoy@gmail.com>
2024-03-01 20:10:32 -0500

    Auto wiring sample created

--- Spring-02-Core-Wiring/src/main/java/com/cydeo/CarApp.java
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/CarApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CarApp {
     public static void main(String[] args) {
         ApplicationContext container = new AnnotationConfigApplicationContext(ConfigCar.class);
-        Car c = container.getBean(Car.class);
+//        Car c = container.getBean(Car.class);
         Person p = container.getBean(Person.class);
         System.out.println("Person " + p.getName() + " drives " + p.getCar()); //car is not set - null
 
