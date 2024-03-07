149ae463c7f737a248e5d48c4e14e3b86861bb6d
Human <vladislav.rogovoy@gmail.com>
2024-03-07 12:14:38 -0500

    secondary configuration with out class definition added

--- Spring-05-Core-review-IoC/src/main/java/bean_practice/AppConfig2.java
+++ Spring-05-Core-review-IoC/src/main/java/bean_practice/AppConfig2.java
@@ -1,4 +1,12 @@
 package bean_practice;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig2 {
+    @Bean("PrimeBean")
+    public String returnString(){return "This is a bean-string";}
+    @Bean("AltBean")
+    public String retunNothing(){return "This is alrernative bean-string";}
 }
