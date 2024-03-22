df68ca318cae63387ead0124a0597fc7a595c7cc
Human <vladislav.rogovoy@gmail.com>
2024-03-22 17:14:35 -0400

    09-Spring-Thymeleaf-lab html page created using bootsraps from https://getbootstrap.com/docs/4.0/getting-started/introduction/

---
+++ Spring-09-Spring-Thymeleaf-lab/src/main/java/com/cydeo/Spring09SpringThymeleafLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringThymeleafLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09SpringThymeleafLabApplication.class, args);
+	}
+
+}
