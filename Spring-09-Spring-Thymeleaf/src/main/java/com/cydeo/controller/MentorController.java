dbe8cdeb39f395dc4fbd4249ee5ae07a6248eb19
Human <vladislav.rogovoy@gmail.com>
2024-03-22 13:00:23 -0400

    09-Spring-Thymeleaf object linked form added mentor class, MentorController, mentor-register.html

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,24 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+@GetMapping("/register")
+    public String showForm(Model model){
+    List<String> batchList = Arrays.asList("JD1", "JD2", "JD3", "JD4");
+
+    model.addAttribute("batchList", batchList);
+    model.addAttribute("mentor", new Mentor());
+    return "mentor/mentor-register";
+}
+
+}
