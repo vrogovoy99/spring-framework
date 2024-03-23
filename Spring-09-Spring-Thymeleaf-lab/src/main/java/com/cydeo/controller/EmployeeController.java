95ca293d4742916e03375c2ebbf05d7a46ca2213
Human <vladislav.rogovoy@gmail.com>
2024-03-23 18:16:31 -0400

    09-Spring-Thymeleaf-lab First Name validation added painfully

--- Spring-09-Spring-Thymeleaf-lab/src/main/java/com/cydeo/controller/EmployeeController.java
+++ Spring-09-Spring-Thymeleaf-lab/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -2,8 +2,10 @@ package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +23,22 @@ public class EmployeeController {
         return "employee/employee-create";
     }
 
+
     @PostMapping("/list")
-    public String captureEmployee(@ModelAttribute("employee") Employee employee, Model model){
+    public String captureEmployee(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
+
+
+            System.out.println("check mark 1");
+        if(bindingResult.hasErrors()) {
+//            in case of validation error repeat createEmployee method to call html page
+//            model.addAttribute("employee", new Employee()); - not needed since we have employee coming in ModeAttribute
+            System.out.println("check mark 2");
+            model.addAttribute("states", DataGenerator.getAllStates());
+
+            return "employee/employee-create";
+        }
+
+        System.out.println("check mark 3");
 
         DataGenerator.saveEmployee(employee);
         model.addAttribute("employees", DataGenerator.readAllEmployees());
--- Spring-09-Spring-Thymeleaf-lab/src/main/java/com/cydeo/model/Employee.java
+++ Spring-09-Spring-Thymeleaf-lab/src/main/java/com/cydeo/model/Employee.java
@@ -1,5 +1,8 @@
 package com.cydeo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,6 +14,9 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 public class Employee {
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     public String firstName;
     public String lastName;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
