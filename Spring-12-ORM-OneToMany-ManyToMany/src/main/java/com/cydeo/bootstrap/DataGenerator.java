e0f37f435a7e1acdd0cb19e14cb2e8f006b3fc99
Human <vladislav.rogovoy@gmail.com>
2024-04-26 12:20:40 -0400

    OneToMany Payment/merchant functionality added

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,8 +1,10 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.enums.Status;
+import com.cydeo.model.Merchant;
 import com.cydeo.model.Payment;
 import com.cydeo.model.PaymentDetail;
+import com.cydeo.repository.MerchantRepository;
 import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,21 +14,30 @@ import java.time.LocalDate;
 @Component
 public class DataGenerator implements CommandLineRunner {
     private final PaymentRepository paymentRepository;
-
-    public DataGenerator(PaymentRepository paymentRepository) {
+    private final MerchantRepository merchantRepository;
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        Merchant merchant1=new Merchant("AmazonSubMerchant", "M123", new BigDecimal("0.25"), new BigDecimal("3.25"), 5) ;
+
+        merchantRepository.save(merchant1);
+
         Payment payment1=new Payment(LocalDate.of(2022,4,19), new BigDecimal("150000"), Status.SUCCESS);
         PaymentDetail paymentDetail1=new PaymentDetail(new BigDecimal("140000"), new BigDecimal("10000"), LocalDate.of(2022,04,24));
         payment1.setPaymentDetail(paymentDetail1);
+        payment1.setMerchant(merchant1);
 
         Payment payment2=new Payment(LocalDate.of(2022,4,25), new BigDecimal("100000"), Status.FAILURE);
         PaymentDetail paymentDetail2=new PaymentDetail(new BigDecimal("90000"), new BigDecimal("5000"), LocalDate.of(2022,04,29));
         payment2.setPaymentDetail(paymentDetail2);
+        payment2.setMerchant(merchant1);
+
+
 
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
