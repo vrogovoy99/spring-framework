9259f1a57ab32bc601228f20b6d91f37df90658f
Human <vladislav.rogovoy@gmail.com>
2024-05-06 14:59:25 -0400

    Spring13 MovieRepository completed

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,8 +1,13 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.enums.State;
+import com.cydeo.enums.Type;
 import com.cydeo.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
@@ -18,14 +23,44 @@ public class DataGenerator implements CommandLineRunner {
 
         callMovieRepository();
 
+        System.out.println("");
+
 
 
     }
 
     private void callMovieRepository(){
         System.out.println("------------------- MOVIE DERIVED QUERIES -------------------");
+
+        System.out.println("\n------------------- movie containing 'The' -------------------");
         movieRepository.findByNameContaining("The")
                 .stream().forEach(p-> System.out.println(p.getName()));
+
+        System.out.println("\n------------------- movie price between 20 and 30 'The' -------------------");
+        movieRepository.findByPriceBetween(20L,30L)
+                .stream().forEach(p-> System.out.println(p.getName() + " - $" + p.getPrice()));
+
+        System.out.println("\n------------------- movie duration between 2 and 3 hours -------------------");
+        movieRepository.findByDurationBetween(120,180)
+                .stream().forEach(p-> System.out.println(p.getName() + " - min:" + p.getDuration()));
+
+        System.out.println("\n------------------- movie released after 2019-12-12  -------------------");
+        movieRepository.findByReleaseDateAfter(LocalDate.of(2019, 12, 12))
+                .stream().forEach(p-> System.out.println(p.getName() + " - release on: " + p.getReleaseDate()));
+
+        System.out.println("\n------------------- movie list all movies with a REGULAR state and ACTIVE type  -------------------");
+        movieRepository.findByTypeAndState(Type.REGULAR, State.ACTIVE)
+                .stream().forEach(p-> System.out.println(p.getName() + " - " + p.getState()  + " - " + p.getType()));
+
+        System.out.println("\n------------------- movies between a range of prices 20-40 -------------------");
+        movieRepository.jpqlPriceRange(20L, 40L)
+                .stream().forEach(p-> System.out.println(p.getName() + " - $" + p.getPrice()));
+
+        System.out.println("\n------------------- movies name list -------------------");
+        movieRepository.jpqlListOfNames()
+                .stream().forEach(System.out::println);
+
+
 
     }
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -1,8 +1,14 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Movie;
+import com.cydeo.enums.State;
+import com.cydeo.enums.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
@@ -14,18 +20,31 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findByNameContaining(String name);
 
     //Write a derived query to list all movies between a range of prices
+    List<Movie> findByPriceBetween(Long startPrice, Long endPrice);
 
     //Write a derived query to list all movies where duration exists in the specific list of duration
+    List<Movie> findByDurationBetween(Integer startDuration, Integer endDuration);
 
     //Write a derived query to list all movies with higher than a specific release date
 
+    List<Movie> findByReleaseDateIsGreaterThanEqualOrderByReleaseDate(LocalDate LocalDatecutOffDate );
+
+    List<Movie> findByReleaseDateAfter(LocalDate cutOffDate );
+
+
+
     //Write a derived query to list all movies with a specific state and type
+    List<Movie> findByTypeAndState(Type type, State state);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movies between a range of prices
+    @Query("SELECT m FROM Movie m WHERE m.price between ?1 and ?2")
+    List<Movie> jpqlPriceRange(Long startPrice, Long endPrice);
 
     //Write a JPQL query that returns all movie names
+    @Query("SELECT m.name FROM Movie m ORDER BY m.name")
+    List<String> jpqlListOfNames();
 
     // ------------------- Native QUERIES ------------------- //
 
