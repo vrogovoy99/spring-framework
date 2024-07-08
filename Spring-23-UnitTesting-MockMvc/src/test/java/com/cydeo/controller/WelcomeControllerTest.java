60091366e2b39f54c23579442a6d281c6d9708b3
Human <vladislav.rogovoy@gmail.com>
2024-07-08 09:12:25 -0400

    welcome mvc test created

---
+++ Spring-23-UnitTesting-MockMvc/src/test/java/com/cydeo/controller/WelcomeControllerTest.java
@@ -0,0 +1,50 @@
+package com.cydeo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(WelcomeController.class)
+class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void welcome() throws Exception {
+        // call welcome endpoint
+        // verify welcome output
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(request).andReturn();
+        assertEquals(200, result.getResponse().getStatus()); // expected status 200, 201 is to cause an error.
+        assertEquals("welcome", result.getResponse().getContentAsString());
+
+    }
+
+
+    @Test
+    void welcome2() throws Exception {
+
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("welcome"))
+                .andReturn();
+    }
+
+}
