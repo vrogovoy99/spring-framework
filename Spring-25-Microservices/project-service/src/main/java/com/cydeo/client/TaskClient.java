c69c20cfe5480f0ded3c70a91f0164e7d86d3df3
Human <vladislav.rogovoy@gmail.com>
2024-09-17 17:00:19 -0400

    Discovery Service runs and User/Task/Project api services successfully connect to it.

--- Spring-25-Microservices/project-service/src/main/java/com/cydeo/client/TaskClient.java
+++ Spring-25-Microservices/project-service/src/main/java/com/cydeo/client/TaskClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(value = "task-service", url = "localhost:8083")
+@FeignClient(value = "task-service")
 public interface TaskClient {
     @GetMapping("/api/v1/task/count/project/{projectCode}")
     ResponseEntity<TaskResponse> getCountsByProject(@PathVariable("projectCode") String projectCode);
